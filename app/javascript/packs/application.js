22c617818633f6bdd7a91c88aac463fb4e216656
Agnes Selvita <a.selvita@gmail.com>
2019-03-29 17:02:19 +0800

    mister cocktails app, search function not done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,3 +17,7 @@
 import 'bootstrap';
 
 console.log('Hello World from Webpacker')
+
+import { initUpdateNavbarOnScroll } from '../components/banner';
+
+initUpdateNavbarOnScroll();
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 require 'open-uri'
 require 'json'
 
-puts "Deleting all records of ingredients..."
-Ingredient.destroy_all if Rails.env.development?
+# puts "Deleting all records of ingredients..."
+# Ingredient.destroy_all if Rails.env.development?
 
-puts "Seeding ingredients..."
+# puts "Seeding ingredients..."
 
-url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
-file = open(url).read
-parsed = JSON.parse(file)
+# url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+# file = open(url).read
+# parsed = JSON.parse(file)
 
-parsed["drinks"].each do |i|
-  Ingredient.create!(name: i["strIngredient1"])
-end
+# parsed["drinks"].each do |i|
+#   Ingredient.create!(name: i["strIngredient1"])
+# end
 
-puts "Created #{Ingredient.count} ingredient(s)"
+# puts "Created #{Ingredient.count} ingredient(s)"
 
 
 puts "Deleting all records of cocktails..."
@@ -27,7 +27,12 @@ file_c = open(url_c).read
 parsed_c = JSON.parse(file_c)
 
 parsed_c["drinks"].first(20).each do |i|
-  Cocktail.create!(name: i["strDrink"], image: i["strDrinkThumb"])
+  c = Cocktail.create!(name: i["strDrink"])
+  c.remote_image_url = i["strDrinkThumb"]
+  c.save
 end
 
 puts "Created #{Cocktail.count} cocktail(s)"
+
+
+# puts "Converted images as remote url"
